4aa0d766a5be85cf07790959e66662c80850f535
Blair <abrgroup@gmail.com>
2020-04-14 14:47:02 -0400

    basic index.js working and running using nodemon

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code away!
+const express = require("express");
+const cors = require("cors");
+const server = express();
+const port = 4008;
+
+server.use(express.json());
+server.use(cors());
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
