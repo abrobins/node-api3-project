d86fe6c52ec85cff47df13676ab216b8967fedcf
Blair <abrgroup@gmail.com>
2020-04-15 20:36:31 -0400

    added heroku env variable

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const postRouter = require("./posts/postRouter");
 const userRouter = require("./users/userRouter");
 
 const server = express();
-const port = 4008;
+const port = process.env.PORT || 4000;
 
 server.use(express.json());
 server.use(cors());
